40e82300aaaab03382de28548017f49fc9452626
rachelch634 <had4059@gmail.com>
2025-02-26 01:47:38 +0200

    Night

--- server/routes/users.js
+++ server/routes/users.js
@@ -12,6 +12,5 @@ router.post('/loginUser', userController.loginUser);
 router.delete('/deleteUser', authenticateToken, userController.deleteUser);
 router.post('/google-login', userController.handleGoogleLogin);
 router.post('/addUserFromForm',userController.addUserFromForm);
-router.post('/logout', authenticateToken, userController.logout);
 
 module.exports = router;
