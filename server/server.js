8bf058319c0ca1ac803a5ab05e15f09842eacb37
rachelch634 <had4059@gmail.com>
2025-02-27 10:26:48 +0200

    XXX

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const userRouter = require('./routes/users');
 const productRouter = require('./routes/products');
 const emailRouter = require('./routes/email');
 const paymentRouter = require('./routes/paymentRoutes');
-require('./gmail-listener');
+// require('./gmail-listener');
 
 const corsOptions = {
   origin: process.env.REACT_URL,
