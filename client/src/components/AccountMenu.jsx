0cc05123dfede776fdfd7b460444ba9f9808d89c
rachelch634 <had4059@gmail.com>
2025-03-11 01:48:29 +0200

    Nevigate

--- client/src/components/AccountMenu.jsx
+++ client/src/components/AccountMenu.jsx
@@ -39,6 +39,8 @@ export default function AccountMenu() {
   const handleLogout = () => {
     setOpenSnackbar(true);
     dispatch(logout());
+    handleClose();
+    navigate('/');
   };
 
   const handleCloseSnackbar = (event, reason) => {
